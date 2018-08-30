44bd1ab07b4e0ce5faf3f9ed29c979c03612fd4c
Charlotte Dunois <github@charuru.moe>
2018-08-30 18:03:10 +0200

    Fix var type [ci skip]

--- src/AudioPlaylist.php
+++ src/AudioPlaylist.php
@@ -11,8 +11,8 @@ namespace CharlotteDunois\Luna;
 
 /**
  * Represents an Audio Playlist.
- * @property string                               $name           The playlist's name, or null for Lavalink v2.
- * @property int                                  $selectedTrack  Which track is selected, or null for Lavalink v2.
+ * @property string|null                          $name           The playlist's name, or null for Lavalink v2.
+ * @property int|null                             $selectedTrack  Which track is selected, or null for Lavalink v2.
  * @property \CharlotteDunois\Collect\Collection  $tracks         The playlist's tracks.
  */
 class AudioPlaylist {
